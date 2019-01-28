1f769c814553e75137c6cba1969151017f9a790f
Stephen Iota <siota001@ucr.edu>
2019-01-28 11:46:01 -0800

    Writing/printing bug fixes for grainsFinal.py

--- grains/grains-creator-script.py
+++ grains/grains-creator-script.py
@@ -8,98 +8,150 @@ print("Enter top material: ")
 top = input()
 print(top)
 print("Enter top material MSat: ")
-top_msat = input()
+top_msat = float(input())
 print("Enter top material AEx: ")
-top_aex = input()
+top_aex = float(input())
 
 print("Enter bottom material: ")
-bottom = input()
+bottom = str(input())
 print("Enter bottom material MSat: ")
-bottom_msat = input()
+bottom_msat = float(input())
 print("Enter bottom material Aex: ")
-bottom_aex = input()
+bottom_aex = float(input())
 
 ## Number of grids
 #print("Enter number of material regions (num-2 must be a factor of 1024): ")
 #num_regions = int(input())
-num_regions = 130
+num_regions = int(10)
 print("Number of regions: " + str(num_regions))
 
+## Class Definitions for mumax Regions
+class mumax_region:
 
+    alpha = "alphaFree"
 
-## Initialize Different Regions in mumax
+    def __init__(self, RegionNumber, RegionName='name', MSat=0.0, AEx=0.0, Shape="cuboid"):
+        self.name = RegionName
+        self.index = RegionNumber
+        self.aex = AEx
+        self.msat = MSat
+        self.shape = Shape
+
 ## Initialize Different Regions in mumax
 my_regions = []
 
 for i in range(num_regions):
     if i < num_regions/2:
-        my_regions.append(mumax_region(i, top + str(i) + "_cuboid", top_msat, top_aex))
+        my_regions.append(mumax_region(i))
+        my_regions[i].name = top + "{}_cuboid".format(i)
+        my_regions[i].aex = top_aex
+        my_regions[i].msat = top_msat
     else:
-        my_regions.append(mumax_region(i, bottom + str(i) + "_cuboid", bottom_msat,bottom_aex))
+        my_regions.append(mumax_region(i))
+        my_regions[i].name = bottom + "{}_cuboid".format(i)
+        my_regions[i].aex = bottom_aex
+        my_regions[i].msat = bottom_msat
+
+
+##########################
+## Positions of regions ##
+##########################
+xsize = (int(num_regions)-2)/4
+
+## top most layer
+my_regions[0].pos = "{} := cuboid(length,width,t_{}).transl(0,0,thickness/2 - t_{}/4)".format(my_regions[0].name,top,top)
+
+## bottom most layer
+my_regions[num_regions-1].pos = "{} := cuboid(length,width,t_{}).transl(0,0,- thickness/2 + 2*t_{}/3)".format(my_regions[num_regions-1].name,bottom,bottom)
+
+## top grains, +x
+j = 1
+for i in range(1,int(num_regions/4)):
+    my_regions[i].pos = "{} := cuboid(length/{},width/2,Zz).transl(length/2 - {}*{}/2,width/2,thickness/2 - t_{}/2)".format(my_regions[i].name,xsize,j,xsize,top)
+    j = j + 2
+
+## top grains, -x
+j = 1
+for i in range(int(num_regions/4),int(num_regions/2)):
+    my_regions[i].pos = "{} := cuboid(length/{},width/2,Zz).transl(length/2 - {}*{}/2,-width/2,thickness/2 - t_{}/2)".format(my_regions[i].name,xsize,j,xsize,top)
+    j = j + 2
+
+## bottom grains, +x
+j = 1
+for i in range(int(num_regions/2),int(3*num_regions/4)):
+    my_regions[i].pos = "{} := cuboid(length/{},width/2,Zz).transl(length/2 - {}*{}/2,width/2,0)".format(my_regions[i].name,xsize,j,xsize,top)
+    j = j + 2
+
+## bottom grains, -x
+j = 1
+for i in range(int(3*num_regions/4),int(num_regions)-1):
+    my_regions[i].pos = "{} := cuboid(length/{},-width/2,Zz).transl(length/2 - {}*{}/2,-width/2,0)".format(my_regions[i].name,xsize,j,xsize,top)
+    j = j + 2
+
+
+## Interlayer Exchange
+print("For interlayer Exchange\n")
+print("Enter mean: ")
+mean = float(input())
+print("Enter std: ")
+std = float(input())
+length = num_regions/2
+
+## generate normal distribution
+exchanges = np.random.normal(loc=mean,scale=std,size=length)
+#print(exchanges)
 
 
 
+## Print Statements
 
-
-
-
-
-## Variables for shape definition
-dims = (num_regions-2)/2
-# create/edit script
 script = open("mumax-script.txt","w")
-script.write("// Variables for shape definition \n")
+
+## Variables for Shape Definition
+script.write("// Variables for Shape Definition\n")
 for i in range(num_regions):
-    if i == 0:
-        my_string = region_names[i] + " := cuboid(length,width,t_" + top + ").transl(0,0,(thickness/2-(1/2)*t_"+ top + "))\n"
-        script.write(my_string)
-    ## top; odd index --> -y-axis
-    elif i < num_regions/2 and i % 2 != 0:
-        my_string = region_names[i] + " := cuboid(length/" + str(dims) +  " , " + "width/" + str(dims) + ", Zz).transl(length/2 - length/" + str(2*dims) +  ",-width/4,zz)\n"
-        script.write(my_string)
-    ## top; even index --> +y-axis
-    elif i < num_regions/2:
-        my_string = region_names[i] + " := cuboid(length/"+ str(dims) +  " , " + "width/" + str(dims) + ", Zz).transl(length/2 - length/" + str(dims*2) + ",width/4,zz)\n"
-        script.write(my_string)
-    # bot; odd index --> -y-axis
-    elif i < num_regions -1 and i % 2 != 0:
-        my_string = region_names[i] + " := cuboid(length/"+ str(dims) +  " , " + "width/" + str(dims) + ", Zz).transl(length/2 - length/" + str(dims*2) + ",-width/4,0)\n"
-        script.write(my_string)
-    ## bot; even index --> +y-axis
-    elif i < num_regions - 1:
-        my_string = region_names[i] + " := cuboid(length/"+ str(dims) +  " , " + "width/" + str(dims) + ", Zz).transl(length/2 - length/" + str(dims*2) + ",width/4,0)\n"
-        script.write(my_string)
-    else:
-        my_string = region_names[i] + " := cuboid(length,width,t_" + bottom + ").transl(0,0,-(thickness/2-(1/2)*t_"+ bottom + "))\n"
-        script.write(my_string)
+    shape = "{}\n".format(my_regions[i].pos)
+    script.write(shape)
+script.write("\n")
 
+## Set Geometry
+script.write("//Set Geometry\n")
+geom = "SetGeom("
+for i in range(num_regions-1):
+    geom += my_regions[i].name + ".add("
+geom += my_regions[num_regions-1].name
+geom += (num_regions)*")" + ("\n \n")
+script.write(geom)
+script.write("\n \n")
 
-# Define Regions
-script.write("\n// Define Regions\n")
+## Define Regions
+script.write("//Define Regions\n")
 for i in range(num_regions):
-    script.write("DefRegion.(" + str(i) + "," + str(region_names[i]) + ")\n")
+    region = "DefRegion({},{})\n".format(my_regions[i].index,my_regions[i].name)
+    script.write(region)
+script.write("\n \n")
 
-
-
-
-# Set MSat & AEx
-script.write("\n// Material Parameters\n")
+## Material Parameters
+script.write("\n //Material Parameters \n")
 for i in range(num_regions):
-        if i < num_regions/2:
-            script.write("MSat.SetRegion("+str(i)+"," + str(top_msat) + ")")
-            script.write("Aex.SetRegion("+str(i)+"," + str(top_aex) + ")")
-        else:
-            script.write("MSat.SetRegion(i," + str(bottom_msat) + ")")
-            script.write("Aex.SetRegion(i," + str(bottom_aex) + ")")
+    mat_param = "Msat.SetRegion({},{}) //{}\n".format(my_regions[i].index,my_regions[i].msat,my_regions[i].name)
+    script.write(mat_param)
+    aex_param = "Aex.SetRegion({},{}) //{}\n".format(my_regions[i].index,my_regions[i].aex,my_regions[i].name)
+    script.write(aex_param)
 
 
+## Alpha
+#script.write("\n//Set Alpha \n")
+#for i in range(num_regions):
+#    my_alph = "alpha.SetRegion({},{})\n".format(my_regions[i].name,my_regions[i].alpha)
+#    script.write(my_alph)
 
-
-
-
-
-
-
-
-
+## Interlayer Exchange
+script.write("\n\\Manipulate Interlayer Exchange\n")
+for i in range(1,int(num_regions/2)):
+    #int_ext1 = "ext_ScaleExchange({},{},{})\n".format(my_regions[i].index,my_regions[i+1].index,exchanges[i])
+    int_ext2 = "ext_ScaleExchange({},{},{})\n".format(my_regions[i].index,my_regions[i+int(num_regions/2 -1)].index,exchanges[i])
+    script.write(int_ext2)
 script.close()
+
+## End script
