b9a51a3781bec8383b72d97604363ec521497217
stepheniota <siota001@ucr.edu>
2019-02-09 14:00:45 -0800

    Sorting algorithm for mumax folders

---
+++ sorting/sort-my-mumax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.5
+## To sort file names created by `mumax-script-creator-field`
+## Stephen Iota
+## 02-2019
+
+import os, sys
+import re
+from glob import glob
+
+
+def alph_to_int(text):
+    return int(text) if text.isdigit() else text
+
+def sorting_key(text):
+    return [ alph_to_int(c) for c in re.split(r'(\d+)', text) ]
+
+file_names = glob("*.out/")
+print(file_names)
+file_names.sort(key=sorting_key)
+print(file_names)
